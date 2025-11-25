f880f4b3615ece6b6695e25668ab32db844b15bd
Pierre-Antoine HIGNARD <pa.hignard@pm.me>
2025-11-25 21:12:40 +0100

    Implementing class preloading in class_mapper.py

--- data/class_mapper.py
+++ data/class_mapper.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, List, Union
 from itertools import count
 import pandas as pd
 from logging import Logger
@@ -25,13 +25,16 @@ class ClassMapping:
     def __getitem__(self, keys: str | pd.Series):
         if isinstance(keys, pd.Series):
             return keys.map(self._get_one_item)
+        elif isinstance(keys, list):
+            return [self._get_one_item(x) for x in keys]
         else:
             return self._get_one_item(keys)
 
     def _get_one_item(self, key: str):
-        if key in self.mapping:
-            return self.mapping[key]
-        else:
+        if not isinstance(key, (str, int)):
+            raise ValueError(f"Value '{key}' is not included in (str, int)")
+
+        if key not in self.mapping:
             self.mapping[key] = next(self._counter)
             self.size += 1
 
@@ -39,7 +42,28 @@ class ClassMapping:
             # if self.logger:
             # self.logger.info(f"Created class mapping '{key}' -> '{self.mapping[key]}' ")
 
-            return self.mapping[key]
+        return self.mapping[key]
+
+    def preload_classes(self, classes: Union[list[str], list[int]]):
+        """
+        Preload classes for the mapper.
+
+        Input:
+            - classes: list[str], a list of all possible class name. Should be int or str.
+
+        Raises:
+            - ValueError, if one or more values is incorrect. All other values are still added.
+        """
+
+        problematic_keys = []
+        for key in classes:
+            try:
+                _ = self._get_one_item(key)
+            except ValueError:
+                problematic_keys.append(key)
+
+        if len(problematic_keys) > 0:
+            raise ValueError(f"Issue with classes {problematic_keys}")
 
     def __str__(self):
         return self.mapping
